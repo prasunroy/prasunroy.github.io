6d6424d88624da141548d418b4bb49b93f45a3c1
Prasun Roy <38404108+prasunroy@users.noreply.github.com>
2020-06-10 20:00:04 +0530

    Updated website

--- static/js/index.js
+++
@@ -1,5 +0,0 @@
-window.onload = function(e) {
-  document.getElementById('pr-navbar-burger').onclick = function(e) {
-    document.getElementById('pr-navbar-menu').classList.toggle('is-active');
-  };
-};
